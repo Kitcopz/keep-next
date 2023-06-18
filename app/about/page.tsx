d6ec7046bcd1272ff457a985f10a0adf974a3075
Kitcopz <132808650+Kitcopz@users.noreply.github.com>
2023-06-18 13:59:35 +0700

    Change login

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/Link'
+
+
+
+export default function Page(){
+    return(
+        <div>
+            <h1>This is the about page</h1>
+            <Link href='/'>Go to homepage</Link>      
+        </div>
+        
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/Link'
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className ='flex flex-col items-center'>
+      <h1 className='text-5xl font-extrabold leading-loose items-center'>Hello, my name is Keep.</h1>
+      <p className="text-3xl font-regular font-serif center">This is my page</p>
+      <Link href='/about'>Go to About Page</Link>
+    </div>
+   
   )
 }
